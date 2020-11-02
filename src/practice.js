3877770cf98a51da2aca961574411e7efa2f374a
ashen-develops <ashenp.designs@gmail.com>
2020-11-02 17:30:44 -0600

    added qry to knexInstance

---
+++ src/practice.js
@@ -0,0 +1,21 @@
+/* eslint-disable indent */
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+});
+console.log('knex and driver installed correctly');
+
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .first()
+  .toQuery();
+  // .then(result => {
+  //   console.log(result);
+  // });
+
+console.log(qry);
